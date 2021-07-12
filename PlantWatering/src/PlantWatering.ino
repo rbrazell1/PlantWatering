05207d04745d42220a70337ee8d3725024216154
Russell Brazell <brazellrussell@gmail.com>
2021-07-12 17:53:10 -0600

    starting code to control pump

---
+++ PlantWatering/src/PlantWatering.ino
@@ -0,0 +1,21 @@
+/*
+ * Project PlantWatering
+ * Description: Code to ensure my plant stays watered
+ * Author: Russell Brazell
+ * Date: 7-12-2021
+ */
+
+#include <IOTTimer.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Grove_Air_quality_Sensor.h>
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+
+}
